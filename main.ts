3072fadb1ec03d4d123403ae5e92b047a63344d4
Iram.Ashfaq <161501174+iram-ashfaq@users.noreply.github.com>
2024-04-14 23:22:46 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,43 @@
+//Question number 23
+// Test 01
+let car = 'subaru';
+console.log("Is car == 'subaru'? I predict True.")
+console.log(car == 'subaru')
+
+// Test 02
+console.log("Is car == 'BMW'? I predict False.")
+console.log(car == 'BMW')
+
+// Test 03
+console.log("Is car === 'subaru'? I predict True.")
+console.log(car === 'subaru')
+
+// Test 04
+console.log("Is car == 'subaru'? I predict False.")
+console.log(car == 'subaru')
+
+// Test 05
+console.log("Is car. length> 0? I predict True.")
+console.log(car. length> 0 )
+
+// Test 06
+console.log("Is car. length> 5? I predict False.")
+console.log(car. length> 5 )
+
+// Test 07
+console.log("Is car == 'Subaru'? I predict False.")
+console.log(car == 'Subaru')
+
+// Test 08
+console.log("Is car.tolowerCase() === 'subaru'? I predict True.")
+console.log(car. toUpperCase() === 'subaru')
+
+// Test 09
+console.log("Is car.tolowerCase() === 'subaru'? I predict True.")
+console.log(car. toLowerCase() === 'subaru');
+
+// Test 10
+console.log("Is car.length == 3? predict True.")
+console.log(car.length ==3)
+
+
